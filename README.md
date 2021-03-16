2cb4d0aeffe24718e7c6fe9a5ae2c634c04e2b1d
suzukiken <suzukiken@gmail.com>
2021-03-16 23:29:24 +0000

    done

--- README.md
+++ README.md
@@ -1,14 +1,7 @@
-# Welcome to your CDK TypeScript project!
+Example CDK TypeScript Project
 
-This is a blank project for TypeScript development with CDK.
+To deploy AWS ApiGateway which responds when apikey is provided.
+The apikey applied to a usage plan which has throttling and quota limits. 
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
-
-## Useful commands
-
- * `npm run build`   compile typescript to js
- * `npm run watch`   watch for changes and compile
- * `npm run test`    perform the jest unit tests
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk synth`       emits the synthesized CloudFormation template
+* `npm install`
+* `cdk deploy`
--- lib/cdkapigwplan-stack.ts
+++ lib/cdkapigwplan-stack.ts
@@ -1,9 +1,62 @@
 import * as cdk from '@aws-cdk/core';
+import * as apigateway from '@aws-cdk/aws-apigateway'
 
 export class CdkapigwplanStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const PREFIX_NAME = id.toLocaleLowerCase().replace('stack', '')
+    
+    const api = new apigateway.RestApi(this, 'api', { 
+      restApiName: PREFIX_NAME + '-api',
+    });
+    
+    const apikey = api.addApiKey('apikey', {
+      apiKeyName: PREFIX_NAME + '-apikey', // 削除してくれない?
+      value: '12345678901234567890', // 20字以上
+    })
+    
+    const plan = api.addUsagePlan('usageplan', {
+      name: 'plan',
+      apiKey: apikey,
+      throttle: {
+        rateLimit: 10,
+        burstLimit: 10
+      },
+      quota: {
+        limit: 200,
+        period: apigateway.Period.DAY
+      }
+    })
+    
+    plan.addApiStage({
+      stage: api.deploymentStage
+    })
+
+    const integration = new apigateway.MockIntegration({
+      passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+      requestTemplates: {
+        'application/json': JSON.stringify({
+          statusCode: 200
+        })
+      },
+      integrationResponses: [{
+        statusCode: '200',
+        responseTemplates: {
+          'text/plain': 'コンニチハ'
+        }
+      }]
+    })
+    
+    api.root.addMethod('GET', integration, {
+      apiKeyRequired: true,
+      methodResponses: [{
+        statusCode: '200',
+        responseModels: {
+          'text/plain': new apigateway.EmptyModel()
+        }
+      }]}
+    )
+    
   }
 }
